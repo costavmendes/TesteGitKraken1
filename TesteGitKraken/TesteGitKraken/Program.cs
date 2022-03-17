0e6a70215377a96256d9b31f17094bb89927426f
Vinicius Mendes <costavm@taginfraestrutura.com.br>
2022-03-17 15:29:35 -0300

    teste

---
+++ TesteGitKraken/TesteGitKraken/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteGitKraken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
