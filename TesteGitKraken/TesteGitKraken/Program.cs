5f814092a52ae25e2a8c2fa3f13d88d3bb3f01d8
Vinicius Mendes <costavm@taginfraestrutura.com.br>
2022-03-17 15:44:08 -0300

    Merge-to-main (#2)
    * teste
    
    * adicionar funcao soma (#1)

---
+++ TesteGitKraken/TesteGitKraken/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TesteGitKraken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        static int Somar(int numero)
+        {
+            int result = numero + 5;
+            return result;
+        }
+    }
+}
